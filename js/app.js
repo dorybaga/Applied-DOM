2a09114b542143a65f03be217365df7f0cc99317
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-02 22:16:07 -1000

    btn 14 green

--- js/app.js
+++ js/app.js
@@ -270,6 +270,9 @@
    * on the setMyLightGreen function
    */
 
+   btn14.addEventListener("click", function(){
+    setMyLightGreen.bind(this) ();
+   });
 
   /*
    * Add a click event listener to btn15
